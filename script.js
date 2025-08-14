22bd4bf3d38c4b4f4133480d9586280c460b870c
NickolasNG <nickolas.dev@hotmail.com>
2025-08-14 14:09:58 -0300

    Adição dos objetos no script.js 14.08.2025

---
+++ script.js
@@ -0,0 +1,47 @@
+const perguntas = [
+    {
+        questao: "Qual o nome da galaxia em que o planeta terra está localizado ?",
+        pergunta:[
+            {text: "Andromeda", correct: false},
+            {text: "Via Láctea", correct: true},
+            {text: "Grande Nuvem de Magalhães ", correct: false},
+            {text: "Ursa Menor", correct: false}
+        ]
+    },
+    {
+        questao: "Qual é o planeta mais próximo do Sol?",
+        pergunta:[
+            {text: "Europa", correct: false},
+            {text: "Jupter", correct: false},
+            {text: "Marte", correct: false},
+            {text: "Mercurio", correct: true}
+        ]
+    },
+        {
+        questao: "Qual o nome do buraco negro no centro de nossa galaxia ?",
+        pergunta:[
+            {text: "TON 618", correct: false},
+            {text: "Phoenix A", correct: false},
+            {text: "Sagittarius A*", correct: true},
+            {text: "Sirius A*", correct: false}
+        ]
+    },
+        {
+        questao: "Qual é o maior planeta do nosso sistema solar?",
+        pergunta:[
+            {text: "Saturno", correct: false},
+            {text: "Terra", correct: false},
+            {text: "Jupter", correct: true},
+            {text: "Urano", correct: false}
+        ]
+    },
+        {
+        questao: "Quantos planetas existem no sistema solar?",
+        pergunta:[
+            {text: "7", correct: false},
+            {text: "6", correct: false},
+            {text: "8", correct: true},
+            {text: "9", correct: false}
+        ]
+    }
+];
